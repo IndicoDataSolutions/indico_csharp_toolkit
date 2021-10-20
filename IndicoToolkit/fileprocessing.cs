06ddfd02d9b387a21e98b5b5227065e4839027b3
Scott <scott.levin@indico.io>
2021-10-20 09:45:37 -0400

    update file processing attributes

--- IndicoToolkit/fileprocessing.cs
+++ IndicoToolkit/fileprocessing.cs
@@ -7,31 +7,29 @@ namespace IndicoToolkit
 {
     public class FileProcessing
     {
-        public List<string> FilePaths; 
-        public List<string> DefaultAcceptedTypes; 
-        
-        public FileProcessing ( List<string> filePathsInput = null)
+        public List<string> filePaths;
+        public List<string> defaultAcceptedTypes = new List<string>() { "*.pdf", "*.doc", "*.docx", "*.tiff", "*.tif", "*.png" };
+
+        public FileProcessing(List<string> filePathsInput = null)
         {
-            FilePaths = filePathsInput ?? new List<string>();
-            DefaultAcceptedTypes = new List<string>() {"*.pdf", "*.doc", "*.docx", "*.tiff", "*.tif", "*.png"};
+            this.filePaths = filePathsInput ?? new List<string>();
         }
 
         public void GetFilePathsFromDir(string pathToDir,
                                         IEnumerable<string> acceptedTypes = null,
                                         bool topDirectoryOnly = true)
         {
-            acceptedTypes = acceptedTypes ?? DefaultAcceptedTypes; 
+            acceptedTypes = acceptedTypes ?? defaultAcceptedTypes;
             SearchOption searchOption = SearchOption.TopDirectoryOnly;
             if (!topDirectoryOnly)
             {
                 searchOption = SearchOption.AllDirectories;
             }
 
-            FilePaths = FileSearch( pathToDir, acceptedTypes.Distinct().ToList(), searchOption);
-            
-            // Print statment about files added or not added
+            this.filePaths = FileSearch(pathToDir, acceptedTypes.Distinct().ToList(), searchOption);
             int dirSize = Directory.GetFiles(pathToDir, "*", searchOption).Length;
-            Console.WriteLine($"Found {FilePaths.Count} valid files and {dirSize - FilePaths.Count} paths with invalid suffixes. Valid suffix:");
+            int filesFound = this.filePaths.Count;
+            Console.WriteLine($"Found {filesFound} valid files and {dirSize - filesFound} paths with invalid suffixes.");
         }
 
         private List<string> FileSearch(string pathToDir,
@@ -47,19 +45,19 @@ namespace IndicoToolkit
             return result;
         }
 
-        public IEnumerable<List<string>> BatchFiles(int batchSize = 10) 
+        public IEnumerable<List<string>> BatchFiles(int batchSize = 10)
         {
-            for (int i = 0; i < FilePaths.Count; i = i + batchSize)
+            for (int i = 0; i < filePaths.Count; i = i + batchSize)
             {
-                int end = Math.Min(batchSize, FilePaths.Count - i);
-                yield return FilePaths.GetRange(i, end);
+                int end = Math.Min(batchSize, filePaths.Count - i);
+                yield return this.filePaths.GetRange(i, end);
             }
         }
 
         public List<string> GetParentDirectoriesOfFilepaths()
         {
             List<string> directories = new List<string>();
-            foreach(var file in FilePaths)
+            foreach (var file in this.filePaths)
             {
                 directories.Add(Directory.GetParent(file).Name);
             }
