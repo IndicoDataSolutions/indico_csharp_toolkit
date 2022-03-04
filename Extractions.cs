4ffede7e964bf5e3727e82ab0e2d5ad8efb58883
Nathanael Shim <nathanaelshim@Nathanaels-MacBook-Pro.local>
2022-03-04 13:06:51 -0800

    filling out extractions class; makes prediction class

--- Extractions.cs
+++ Extractions.cs
@@ -14,86 +14,133 @@ namespace IndicoToolkit
     /// Class <c>Extractions</c> provides functionality for common extraction prediction use cases.
     /// </summary>
     public class Extractions {
-        internal List<Dictionary<string, string>> preds = new List<Dictionary<string, string>>();
-        public List<Dictionary<string, string>> removed_predictions = new List<Dictionary<string, string>>();
+        internal List<Prediction> preds = new List<Prediction>();
+        public List<Prediction> removed_predictions = new List<Prediction>();
         
-        /// def __init__(self, predictions: List[dict]):
-        public Extractions(List<Dictionary<string, string>> predictions){
+        public Extractions(List<Prediction> predictions)
+        {
             this.preds = predictions;
         }
 
-        /*
-        @property
-        def to_dict_by_label(self) -> Dict[str, list]:
-        */
         /// <summary>
         /// Method <c>toDictByLabel</c> generates a dictionary where key is label string and value is list of all predictions of that label.
         /// </summary>
-        public Dictionary<string, List<Dictionary<string, string>>> toDictByLabel() {
-            /* TODO */
-            return ;
+        public Dictionary<string, List<Prediction>> toDictByLabel()
+        {
+            prediction_label_map = new Dictionary<string, List<Prediction>>();
+            for (int i = 0; i < preds.Count; i++) 
+            {
+                prediction_label_map[preds[i]["label"]].Append(preds[i]);
+            }
+            return prediction_label_map;
         }
-
-        /*
-        @property
-        def num_predictions(self) -> int:
-        */
+        
         /// <summary>
         /// Method <c>numPredictions</c> returns number of predictions.
         /// </summary>
-        public int numPredictions() {
-            return self.preds.Count;
+        public int numPredictions() 
+        {
+            return preds.Count; 
         }
 
-        /*
-        @property
-        def remove_by_confidence(self, confidence: float = 0.95, labels: List[str] = None):
-        */
         /// <summary>
         /// Method <c>removeByConfidence</c> removes predictions that are less than given confidence.
         /// </summary>
-        public void removeByConfidence(float confidence = 0.95f, List<string> labels = null) {
-            /* TODO */
-            return ;
+        public void removeByConfidence(float confidence = 0.95f, List<string> labels = null) 
+        {
+            List<Prediction> high_conf_preds = new List<Prediction>();
+            for (int i = 0; i < preds.Count; i++) {
+                string label = preds[i]["label"];
+                if (labels != null && !labels.Contains(label)) {
+                    high_conf_preds.Append(preds[i]);
+                } else if (preds[i]["confidence"][label] >= confidence) {
+                    high_conf_preds.Append(preds[i]);
+                } else {
+                    removed_predictions.Append(preds[i]);
+                }
+            }
+            preds = high_conf_preds;
         }
 
-        
-        
-        /*
-        @staticmethod
-        def _set_confidence_key_to_max_value(preds):
-        */
-        internal static List<> setConfidenceKeyToMaxValue(List<> preds) {
-            /* TODO */
-            return ;
-        }
-
-        /*
-        @staticmethod
-        def get_label_set(predictions: List[dict]) -> Set[str]:
-        */
         /// <summary>
-        /// Method <c>getLabelSet</c> gets the included labels from a list of predictions.
+        /// Method <c>removeExceptMaxConfidence</c> removes all predictions except the highest confidence within each specified class.
         /// </summary>
-        public static HashSet<string> getLabelSet(List<Dictionary<string, string>> predictions) {
+        public void removeExceptMaxConfidence(List<string> labels) 
+        {
             /* TODO */
             return ;
         }
         
-        /*
-        def __len__(self):
-        */
+        /// <summary>
+        /// Method <c>removeKeys</c> removes specified keys from prediction dictionaries.
+        /// </summary>
+        public void removeKeys(List<string> keys_to_remove = ["start", "end"]) 
+        {
+            for (int i = 0; i < preds.Count; i++) {
+                Prediction pred = preds[i];
+                for (int j = 0; j < keys_to_remove.Count; j++) {
+                    string key = keys[j];
+                    pred.Remove(key);
+                }
+            }
+        }
 
+        /// <summary>
+        /// Method <c>removeAllByLabel</c> removes all predictions in list of labels.
+        /// </summary>
+        public void removeAllByLabel(List<string> labels)
+        {
+            List<Prediction> newPreds = new List<Prediction>();
+            for (int i = 0; i < preds.Count; i++) {
+                Prediction pred = preds[i];
+                if (labels.Contains(pred["label"])) {
+                    removed_predictions.Append(pred);
+                } else {
+                    newPreds.Append(pred);
+                }
+            }
+            preds = newPreds;
+        }
 
-        /*
-        def to_csv(
-            self,
-            save_path: str,
-            filename: str = "",
-            append_if_exists: bool = True,
-            include_start_end: bool = False,
-        ) -> None
-        */
+        /// <summary>
+        /// Method <c>removeHumanAddedPredictions</c> removes predictions that were not added by the model (i.e. added by scripted or human review).
+        /// </summary>
+        public void removeHumanAddedPredictions()
+        {
+            List<Prediction> newPreds = new List<Prediction>();
+            for (int i = 0; i < preds.Count; i++) {
+                Prediction pred = preds[i];
+                if (!isManuallyAddedPrediction(pred)) {
+                    newPreds.Append(pred);
+                }
+            }
+            preds = newPreds;
+        }
+
+        /// <summary>
+        /// Method <c>isManuallyAddedPrediction</c> checks if prediction has been manually added.
+        /// </summary>
+        public static bool isManuallyAddedPrediction(Prediction prediction)
+        {
+            if (prediction["start"] is int && prediction["end"] is int) {
+                if (prediction["end"] > prediction["start"]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Method <c>selectMaxConfidence</c> gets the highest confidence prediction for a given field.
+        /// </summary>
+        internal Prediction selectMaxConfidence(string label) {
+            /* TODO */
+            return ;
+        }
+
+        /// <summary>
+        /// Method <c>toCSV</c> writes three column CSV ('confidence', 'label', 'text')
+        /// </summary>
         public void toCSV(string save_path, string filename = "", bool append_if_exists = true, bool include_start_end = false) {
             /* TODO */
             return ;
---
+++ IndicoToolkit/Types/Prediction.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace IndicoToolkit.Types
+{
+    public class Prediction
+    {
+        public JObject value { get; set; }
+    }
+
+    
+}
