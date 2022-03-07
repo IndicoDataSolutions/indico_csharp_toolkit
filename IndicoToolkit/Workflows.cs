05f49e3bea3dbb0f042cd4421a73ce6826e227d5
Scott <scott.levin@indico.io>
2022-03-07 15:29:02 -0500

    fixed tests

---
+++ IndicoToolkit/Workflows.cs
@@ -0,0 +1,13 @@
+using Indico;
+
+namespace IndicoToolkit
+{
+    public class Workflows
+    {
+        public IndicoClient client;
+        public Workflows(IndicoClient _client)
+        {
+            this.client = _client;
+        }
+    }
+}
